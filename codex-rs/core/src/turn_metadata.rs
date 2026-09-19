ee2761f07f3a5eff602906f0e9aa8008803f0063
CodexMulti Release <codexmulti@localhost>
2026-09-19 16:48:27 +0000

    Attribute first model requests before optional Git enrichment finishes

--- codex-rs/core/src/turn_metadata.rs
+++ codex-rs/core/src/turn_metadata.rs
@@ -101,6 +101,7 @@ pub async fn detached_memory_responses_metadata(
         thread_source: Some(ThreadSource::MemoryConsolidation),
         subagent_header: subagent_header_value(session_source),
         sandbox: sandbox.map(ToString::to_string),
+        local_usage_workspace: Some(cwd.clone()),
         workspaces: memory_workspaces(
             cwd,
             thread_manager.git_root_discovery().discover(cwd.clone()),
@@ -441,6 +442,7 @@ impl TurnMetadataState {
             auto_review_enabled: Some(self.auto_review_enabled),
             node_repl_auto_review_required: Some(self.node_repl_auto_review_required),
             node_repl_disabled: Some(self.node_repl_disabled),
+            local_usage_workspace: Some(self.cwd.clone()),
             workspaces: self.current_workspaces(),
             tool_namespaces_info: self
                 .tool_namespaces_info
